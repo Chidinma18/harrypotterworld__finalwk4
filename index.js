7311432a09e0783e3b0099c86fbeb4a3c66e215c
Chidinma18 <mariamwill01@aol.com>
2025-11-14 00:58:00 -0800

    starting w new api

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// 
+// https://potterhead-api.vercel.app/api/books
+async function main(){
+    const characters = await fetch("https://potterhead-api.vercel.app/api/books");
+    const charactersData = await characters.json();
+    console.log(charactersData)
+}
+
+
+console.log(main());
