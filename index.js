9de92b5a1aec3d91a47c7c8cbe4252074a5221d1
Chidinma18 <mariamwill01@aol.com>
2026-01-03 04:54:39 -0800

    created function for books, now working on rerouting index.html to another .html(maybe books.html if created?

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 // https://potterhead-api.vercel.app/api/books
+// https://potterhead-api.vercel.app/api/houses
+// https://potterhead-api.vercel.app/api/characters
 
+const bookListEl = document.querySelector(".book-list");
 
 async function main() {
     const books = await fetch("https://potterhead-api.vercel.app/api/books");
     const booksData = await books.json();
-    const bookListEl = document.querySelector(".book-list");
-    console.log(booksData);
-
-    bookListEl.innerHTML = booksData
-        .map(
-            (book) => `<div class="book-card">
-        <div class="book-card__container">
-            <h3> ${book.title} </h3>
-            <p><b>Cover</b> <img src="${book.cover} alt=""></a> </p>
-            <h5><b>Summary: </b> ${book.summary} </h5>
-            <h5><b>Release Date: </b>" ${book.release_date} "</h5>
-            <h5><b>Pages: </b> ${book.pages} </h5>
-            <h5><b>Wiki: </b> <a href=" ${book.wiki} " target="_blank"> ${book.wiki} </a></h5>
-        </div>
-    </div>`
-    )
-    .join("");
+    bookListEl.innerHTML = booksData.map((book) => bookHTML(book)).join("");
 }
 
+main();
 
-console.log(main());
+function showBookPlot(serial) {
+    console.log(serial);
+}
+
+function bookHTML(book) {
+    return `<div class="book-card" onclick="showBookPlot(${book.serial})">
+    <div class="book-card__container">
+      <h3>${book.title}</h3>
+      <h5><b>Summary: </b> ${book.summary} </h5>
+      <h5><b>Release Date: </b>${book.release_date}</h5>
+      <h5><b>Pages: </b> ${book.pages} </h5>
+      <h5><b>Wiki: </b> <a href="${book.wiki}" target="_blank">
+      ${book.wiki} 
+      </a></h5>
+    </div>
+</div>`;
+};
